ce83499589508e844f9da8b483dc989ffb4da554
AnneMbulwa <annembulwa602@gmail.com>
2023-11-21 11:30:13 +0300

    sorting algorithms

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ *selection_sort - sorts an array of integers in ascending order
+ *using the Selection sort algorithm
+ *@array: array
+ *@size: size of array
+ *Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t a, b;
+	int m_index, tmp;
+
+	if (array == NULL || size < 2)
+		return;
+	for (a = 0; a < size - 1; a++)
+	{
+		m_index = a;
+		for (b = a + 1; b < size; b++)
+			if (array[b] < array[m_index])
+				m_index = b;
+
+		if (m_index != array[a])
+		{
+			tmp = array[a];
+			array[a] = array[m_index];
+			array[m_index] = tmp;
+			print_array(array, size);
+		}
+	}
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,79 @@
+#include "sort.h"
+
+/**
+ *swap - function that swaps two elements
+ *@array: array sort
+ *@p: first element
+ *@q: second element
+ *@size: size of array
+ */
+void swap(int * array, int *p, int *q, size_t size)
+{
+	int temp;
+
+	temp = *p;
+	*p = *q;
+	*q = temp;
+	print_array(array, size);
+}
+
+/**
+ *partition - partitions the elements in the array
+ *@array: array sort
+ *@low: low element value
+ *@high: high value element
+ *@size: size of array
+ *Return: pivot position
+ */
+int partition (int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int a = (low - 1);
+	int b;
+
+	for (b = low; b <= high; b++)
+	{
+		if (array[b] < pivot)
+			if (a != b)
+			{
+				swap(array, &array[a], &array[b], size);
+				a++;
+			}
+	}
+	if (a != high)
+		swap(array, &array[a + 1], &array[high], size);
+
+	return (a + 1);
+}
+
+/**
+ *quickSort - sorts the elements
+ *@array: array sort
+ *@low: low element value
+ *@high: high element value
+ *@size: size of array
+ *Return: void
+ */
+void quickSort(int *array, int low, int high, size_t size)
+{
+	int pi = 0;
+
+	if (low <= high)
+		pi = partition(array, low, high, size);
+	quickSort(array, low, pi - 1, size);
+	quickSort(array, pi + 1, high, size);
+}
+
+/**
+ *quick_sort - sorts an array of integers in ascending order
+ *using the Quick sort algorithm
+ *@array: array sort
+ *@size: size of array
+ *Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quickSort(array, 0, size - 1, size);
+}
