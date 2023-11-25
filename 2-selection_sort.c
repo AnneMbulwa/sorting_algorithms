1194f017e0331c337ad937ac822a1b4d2a619144
AnneMbulwa <annembulwa602@gmail.com>
2023-11-25 12:49:29 +0300

    sorting algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,7 +14,7 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	for (a = 0; a < size - 1; a++)
+	for (a = 0; a < size; a++)
 	{
 		m_index = a;
 		for (b = a + 1; b < size; b++)
