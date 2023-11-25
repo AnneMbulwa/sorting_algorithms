806656c4b90bfb78b98cd8019c62109732207160
AnneMbulwa <annembulwa602@gmail.com>
2023-11-25 18:57:37 +0300

    sorting algorithms

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,15 +1,14 @@
 #include "sort.h"
 
 /**
- *find_max - finds and gets the maximum value im the array
- *@array: array sort
- *@size: size of array
- *Return: maximum value(int)
+ * find_max - finds and gets the maximum value in the array
+ * @array: array to sort
+ * @size: size of array
+ * Return: maximum value (int)
  */
 int find_max(int *array, int size)
 {
 	int max, a;
-
 	for (max = array[0], a = 1; a < size; a++)
 	{
 		if (array[a] > max)
@@ -19,11 +18,11 @@ int find_max(int *array, int size)
 }
 
 /**
- *counting_sort - sorts an array of integers in ascending order
- *using the Counting sort algorithm
- *@array: array sort
- *@size: size of array
- *Return: void
+ * counting_sort - sorts an array of integers in ascending order
+ * using the Counting sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
  */
 void counting_sort(int *array, size_t size)
 {
@@ -31,26 +30,31 @@ void counting_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	countArray = malloc(sizeof(int) * size);
-	if (countArray == NULL)
-		return;
 	max = find_max(array, size);
-	arr = malloc((max + 1) * sizeof(int));
+
+	countArray = malloc((max + 1) * sizeof(int));
+	if (countArray == NULL)
+        	return;
+
+	arr = malloc(size * sizeof(int));
 	if (arr == NULL)
 	{
 		free(countArray);
 		return;
 	}
 
-	for (a = 0; a < (max + 1); a++)
+	for (a = 0; a <= max; a++)
 		countArray[a] = 0;
-	for (a = 0; a < (int)size; a++)
-		countArray[array[a]] += 1;
-	for (a = 1; a < (max + 1); a++)
-		countArray[a] += countArray[a - 1];
-	print_array(countArray, max + 1);
 
 	for (a = 0; a < (int)size; a++)
+		countArray[array[a]] += 1;
+
+	for (a = 1; a <= max; a++)
+		countArray[a] += countArray[a - 1];
+
+	print_array(countArray, max + 1);
+
+	for (a = size - 1; a >= 0; a--)
 	{
 		arr[countArray[array[a]] - 1] = array[a];
 		countArray[array[a]] -= 1;
